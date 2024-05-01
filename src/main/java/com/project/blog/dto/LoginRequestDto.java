bce9584566a0193bdf3727e5a23a0144ccb37a08
Kim Taejin <rlaxowls0504@gmail.com>
2024-05-02 01:22:07 +0900

    feat: JwtAuthenticationFilter 생성

---
+++ src/main/java/com/project/blog/dto/LoginRequestDto.java
@@ -0,0 +1,11 @@
+package com.project.blog.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequestDto {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/project/blog/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,71 @@
+package com.project.blog.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.blog.dto.LoginRequestDto;
+import com.project.blog.entity.UserRoleEnum;
+import com.project.blog.jwt.JwtUtil;
+import com.project.blog.security.UserDetailsImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+@Slf4j(topic = "로그인 및 JWT 생성")
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+
+        setFilterProcessesUrl("/api/user/login");
+    }
+
+    // UsernamePasswordAuthenticationToken 생성
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+        try {
+            LoginRequestDto requestDto = new ObjectMapper().readValue(request.getInputStream(), LoginRequestDto.class);
+
+            return getAuthenticationManager().authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            requestDto.getUsername(),
+                            requestDto.getPassword(),
+                            null
+                    )
+            );
+        } catch (IOException e) {
+            log.error(e.getMessage());
+
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    // JWT 토큰 생성 후 클라이언트에게 Header로 전송
+    @Override
+    public void successfulAuthentication(HttpServletRequest request,
+                                         HttpServletResponse response,
+                                         FilterChain chain,
+                                         Authentication authResult) {
+        String username = ((UserDetailsImpl) authResult.getPrincipal()).getUsername();
+        UserRoleEnum role = ((UserDetailsImpl) authResult.getPrincipal()).getUser().getRole();
+
+        String token  = jwtUtil.createToken(username, role);
+
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, token);
+    }
+
+    @Override
+    public void unsuccessfulAuthentication(HttpServletRequest request,
+                                           HttpServletResponse response,
+                                           AuthenticationException failed) {
+        response.setStatus(401);
+    }
+}
