4cf39d3d2404c15280d36dbfb232a7061a9f1dc2
Kim Taejin <rlaxowls0504@gmail.com>
2024-05-02 01:07:28 +0900

    feat: UserDetailsImpl, UserDetailsServiceImpl 생성

---
+++ src/main/java/com/project/blog/security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.project.blog.security;
+
+import com.project.blog.entity.User;
+import com.project.blog.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userOptional = userRepository.findByUsername(username);
+        User user = userOptional.orElseThrow(() ->
+                new UsernameNotFoundException("Not Found " + username)
+        );
+
+        return new UserDetailsImpl(user);
+    }
+}
